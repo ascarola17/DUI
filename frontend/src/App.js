f8006b548c245d3b96d894e666de3f13f871a316
Junior <juniorlawsonv@gmail.com>
2024-09-28 00:36:18 -0600

    fixed env file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+const center = {
+  lat: 31.7619, // Example latitude (El Paso)
+  lng: -106.4850 // Example longitude (El Paso)
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>My Google Maps Integration</h1>
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
         >
-          Learn React
-        </a>
-      </header>
+          {/* Other child components like markers can be added here */}
+        </GoogleMap>
+      </LoadScript>
     </div>
   );
 }
